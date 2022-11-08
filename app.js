9ac7b58575f21f33554d98d8fcf6b4db4b215691
Meka827 <javy.meka@gmail.com>
2022-11-08 11:13:09 -0600

    worked up to feature 6, stating w/ feature 7

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-play(); // This is your main function that runs when the page loads
+const player = prompt("Hey! What's your name!")
+var count = 0;
+let history: {};
 
-function play(){
-   
+
+function play() {  
+  const SECRET_NUMBER = Math.floor(Math.random() * 10) + 1;
+  var guessArray = [];
+  while(true) {
+    var guess = prompt(`OK ${player}, Guess a number!`);
+    count++
+    guessArray.push(guess);
+    if (guess === null) {
+      alert("Goodbye!");
+      break;
+    }
+
+    var numGuess = Number(guess);
+
+    if(Number.isNaN(numGuess)) {
+      alert("Please enter a valid number");
+    } else if (numGuess > SECRET_NUMBER) {
+      alert(`Sorry ${player}, Guess Lower`);
+    } else if (numGuess < SECRET_NUMBER) {
+      alert(`Sorry ${player}, Guess Higher`);
+    } else {
+      alert(`That's Correct ${player}! Your previous guesses are ${formatArray(guessArray)}!`);
+      var playAgain = prompt('Do you want to play again?')
+      if (playAgain.toLowerCase() === 'yes') {
+        play();
+      } else {
+        break;
+      }
+    }
+  }
+
 }
 
-function playAgain(){
-    
+var formatArray = function(array) {
+  var string = '';
+  if (array.length === 1) {
+    string = array.slice(0).join()
+  } else if (array.length === 2) {
+    string = array.join(' and ')
+  } else if(array.length > 2) {
+    string = array.slice(0, -1).join(', ') + ', and ' + array.slice(-1);
+  }
+  return string;
 }
+
+play();
