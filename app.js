ae495a54834a634c19eb9fa302f72ba877a97e3b
Meka827 <javy.meka@gmail.com>
2022-11-08 21:00:20 -0600

    finished product

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
-const player = prompt("Hey! What's your name!")
-var count = 0;
-let history: {};
+const history = {};
 
 
-function play() {  
-  const SECRET_NUMBER = Math.floor(Math.random() * 10) + 1;
-  var guessArray = [];
-  while(true) {
-    var guess = prompt(`OK ${player}, Guess a number!`);
-    count++
-    guessArray.push(guess);
-    if (guess === null) {
-      alert("Goodbye!");
-      break;
-    }
+function play() {
+   let guessCounter = 1;
+   const player = prompt("Hey! What's your name!");
+   let correctNum = Math.floor(Math.random() * 10) + 1;
+   var count = 0;
+   //var guessArray = [];
 
-    var numGuess = Number(guess);
-
-    if(Number.isNaN(numGuess)) {
-      alert("Please enter a valid number");
-    } else if (numGuess > SECRET_NUMBER) {
-      alert(`Sorry ${player}, Guess Lower`);
-    } else if (numGuess < SECRET_NUMBER) {
-      alert(`Sorry ${player}, Guess Higher`);
-    } else {
-      alert(`That's Correct ${player}! Your previous guesses are ${formatArray(guessArray)}!`);
-      var playAgain = prompt('Do you want to play again?')
-      if (playAgain.toLowerCase() === 'yes') {
-        play();
-      } else {
-        break;
+   while (true) {
+      const guess = prompt(`OK ${player}, Guess a number!`);
+      if (guess === null) {
+         alert("Goodbye!");
+         break;
       }
-    }
-  }
+      let numGuess = Number(guess);
 
+      if (Number.isNaN(guess)) {
+         alert("Please enter Valid Number");
+         numGuess++;
+      } else if (guess < correctNum) {
+         alert(`Sorry ${player}, Guess Higher!`);
+         numGuess++;
+      } else if (guess > correctNum) {
+         alert(`Sorry ${player}, Guess Lower!`);
+         numGuess++;
+      } else {
+
+         if (history[player] === undefined) {
+            alert(`${player} Correct! It only took you ${numGuess} guesses!}`);
+         } else {
+            const previousAtt = history[player];
+            const difference = previousAtt - numGuess;
+
+            if (difference < 0) {
+               alert(`That's Correct ${player}! You did better in your last game by ${Math.abs(difference)} fewer guesses.`);
+            } else if (difference > 0) {
+               alert(`That's Correct ${player}! You beat you previous attempt by ${difference} fewer guesses.`);
+            } else {
+               alert(`That's Correct ${player}! You tied your previous attempt.`);
+            }
+         }
+         //alert(${player} Correct! It only took you ${count} guesses! \n Previous Entries: ${guessArray});
+         history[player] = numGuess;
+         const goAgain = prompt("Do you want to play again?");
+         if (goAgain.toLowerCase() === "yes") {
+            play();
+         } else if(goAgain.toLowerCase() === "no"){
+            break;
+         }
+      }
+   };
 }
-
-var formatArray = function(array) {
-  var string = '';
-  if (array.length === 1) {
-    string = array.slice(0).join()
-  } else if (array.length === 2) {
-    string = array.join(' and ')
-  } else if(array.length > 2) {
-    string = array.slice(0, -1).join(', ') + ', and ' + array.slice(-1);
-  }
-  return string;
-}
-
 play();
